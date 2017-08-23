edfa6db20d0965cd948baddf88d621b039a24a84
João Pedro <jp-20011973@hotmail.com>
2017-08-22 23:15:16 -0300

    Add files via upload

---
+++ Truco/js/script.js
@@ -0,0 +1,32 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
+let nipes = ['copas', 'ouro', 'paus', 'espada'];
+let i = 0;
+var ponto1 = 0;
+var ponto2 = 0;
+var pontos = 30;
+var bt = document.querySelector('#iniciar')
+var mao = [];
+var maott = [];
+function distribuir(){
+  mao = [];
+    for(var i = 0; i < 3; i++){
+      let n = numeros[Math.floor(Math.random()*numeros.length)];
+      let np = nipes[Math.floor(Math.random()*nipes.length)];
+      let maot = [ n, np];
+      if(maott.indexOf(maot) < 0){
+        mao.unshift(maot);
+        maott.unshift(maot);
+      }
+    }
+    return mao
+}
+var mao1 = distribuir();
+var mao2 = distribuir();
+document.write(mao1[0]);
+document.write(mao1[1]);
+document.write(mao1[2]);
+document.write(mao2[0]);
+document.write(mao2[1]);
+document.write(mao2[2]);
+
+console.log(mao);
