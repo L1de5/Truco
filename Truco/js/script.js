a4cf67ddc4a1a3779b621417b53e2620418acd1f
Rodrigo Martins <contawarface750@gmail.com>
2017-08-22 23:27:54 -0300

    Update script.js

--- Truco/js/script.js
+++ Truco/js/script.js
@@ -30,3 +30,33 @@ document.write(mao2[1]);
 document.write(mao2[2]);
 
 console.log(mao);
+
+// codigao
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
+let nipes = ['copas', 'ouro', 'paus', 'espada'];
+var ponto1 = ponto2 = 0;
+var pontos = 30;
+var bt = document.querySelector('#iniciar');
+var mao = [] = maott = [];
+
+var sorteia = ()=> ""+numeros[Math.floor(Math.random()*numeros.length)] + nipes[Math.floor(Math.random()*nipes.length)];
+
+var distribuir = function  (){
+  mao = [];
+    for(var i = 0; i < 3; i++){
+      let carta = sorteia();
+      if(maott.indexOf(carta) == -1)maott+=(","+carta);
+    }
+    return mao
+}
+var mao1 = distribuir();
+var mao2 = distribuir();
+document.write(mao1[0]);
+document.write(mao1[1]);
+document.write(mao1[2]);
+document.write(mao2[0]);
+document.write(mao2[1]);
+document.write(mao2[2]);
+
+console.log(mao);
